f69b3188c05fa22b553c5b54ff33d6b87afe4753
dali <daki9981@qq.com>
2021-08-13 13:56:06 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/atguigu/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
