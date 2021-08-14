1ab1948f46db5f135f890177339688de54a2eeab
dali <daki9981@qq.com>
2021-08-14 13:58:09 +0800

    gulimall-wall谷粒商城-仓储服务相关配置文件

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareInfoDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ware.dao;
+
+import com.atguigu.gulimall.ware.entity.WareInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 仓库信息
+ * 
+ * @author ÍõÈ½ê¿
+ * @email daki9981@qq.com
+ * @date 2021-08-14 13:45:49
+ */
+@Mapper
+public interface WareInfoDao extends BaseMapper<WareInfoEntity> {
+	
+}
---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareSkuDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ware.dao;
+
+import com.atguigu.gulimall.ware.entity.WareSkuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品库存
+ * 
+ * @author ÍõÈ½ê¿
+ * @email daki9981@qq.com
+ * @date 2021-08-14 13:45:49
+ */
+@Mapper
+public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
+	
+}
