8e4d131388dd7f1f14cb7d3da4b57c14df8caddf
dali <daki9981@qq.com>
2021-08-14 13:32:00 +0800

    通过人人开源项目逆向工程生成：谷粒商城-会员服务gulimall-member

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/entity/UndoLogEntity.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/entity/UndoLogEntity.java
@@ -39,7 +39,8 @@ public class UndoLogEntity implements Serializable {
 	/**
 	 * 
 	 */
-	private Longblob rollbackInfo;
+//	private Longblob rollbackInfo;
+	private byte[] rollbackInfo; //TODO
 	/**
 	 * 
 	 */
