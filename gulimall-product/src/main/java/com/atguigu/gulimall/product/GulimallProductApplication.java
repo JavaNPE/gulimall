be05ec6ab0775b16deef91617a52b6604ba43e3b
dali <daki9981@qq.com>
2021-08-20 18:34:10 +0800

    48、商品服务-API-三级分类查询-树形展示三级分类数据：将精确的路由规则放置到模糊的路由规则的前面，将gulimall-product注册到naocs中

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1.整合MyBatis-Plus
@@ -20,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *            1).使用@MapperScan
  *            2).告诉MyBatis-Plus，sql映射文件位置在哪里
  */
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
