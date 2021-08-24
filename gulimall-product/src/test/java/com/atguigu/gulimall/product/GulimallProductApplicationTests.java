b215cbd7a0e6cff9dd4e81258ffe41498e3b9ca7
dali <daki9981@qq.com>
2021-08-24 17:17:08 +0800

    方式二：文件上传使用SpringCloud Alibaba来管理OSS （推荐）

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,22 +1,59 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-//import org.junit.jupiter.api.Test;
+
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.net.www.content.image.png;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
 
+    @Autowired
+    OSSClient ossClient;
     @Test
-    void contextLoads() {
+    public void testUpload() throws FileNotFoundException {
+
+/*        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tQJe43xkmKionyVWfkH";
+        String accessKeySecret = "NYJesMLZJcVdFvhVx5bjEPw20vBe2z";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);*/
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\FFOutput\\4.png");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-dali", "4.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成...");
+    }
+
+    @Test
+    public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
 //        brandEntity.setBrandId(6L);
 //        brandEntity.setDescript("华为天下第一");
@@ -34,7 +71,7 @@ class GulimallProductApplicationTests {
 //        for (BrandEntity entity : list) {
 //            System.out.println(entity);
 //        }
-        list.forEach((item)->{
+        list.forEach((item) -> {
             System.out.println(item);
         });
     }
