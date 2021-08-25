a3fd0763b55e131e6b4af3315e41f795dabcdb20
dali <daki9981@qq.com>
2021-08-25 14:09:04 +0800

    63、商品服务-API-品牌管理- OSS获取服务端签名：测试案例

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,50 @@
 package com.atguigu.gulimall.thirdparty;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import com.aliyun.oss.OSSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallThirdPartyApplicationTests {
+public class GulimallThirdPartyApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+
+/*        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tQJe43xkmKionyVWfkH";
+        String accessKeySecret = "NYJesMLZJcVdFvhVx5bjEPw20vBe2z";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);*/
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\FFOutput\\8.png");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-dali", "8.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成...");
+    }
 }
