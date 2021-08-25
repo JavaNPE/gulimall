4188b98fc9485ead6999243f7b79166477a938e1
dali <daki9981@qq.com>
2021-08-26 00:17:22 +0800

    66、商品服务-API-品牌管理- JSR303数据校验——后端校验 | 以及相关校验注解：@NotNull @NotEmpty等 | @Valid:告诉SpringMVC我们需要使用校验

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -25,6 +25,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * 1)、配置全局的逻辑删除规则(省略)
  * 2)、配置逻辑删除的组件Bean (省略)
  * 3)、给Bean加上逻辑删除注解@TableLogic
+ *
+ *
+ * 3、ISR303
+ *      1)、给Bean添加校验注解: javax. val idation. constraints,并定义自己的message提示
+ *      2)、开启校验功能@Valid
+ *          效果:校验错误以后会有默认的响应;
+ *      3)、给校验的bean后紧跟一个BindingResult, 就可以获取到校验的结果
  */
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -5,11 +5,15 @@ import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
- * 
+ *
  * @author ÍõÈ½ê¿
  * @email daki9981@qq.com
  * @date 2021-08-13 19:53:21
@@ -17,36 +21,43 @@ import lombok.Data;
 @Data
 @TableName("pms_brand")
 public class BrandEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 品牌id
-	 */
-	@TableId
-	private Long brandId;
-	/**
-	 * 品牌名
-	 */
-	private String name;
-	/**
-	 * 品牌logo地址
-	 */
-	private String logo;
-	/**
-	 * 介绍
-	 */
-	private String descript;
-	/**
-	 * 显示状态[0-不显示；1-显示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 检索首字母
-	 */
-	private String firstLetter;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
+    /**
+     * 品牌id
+     */
+    @TableId
+    private Long brandId;
+    /**
+     * 品牌名
+     */
+    @NotBlank(message = "品牌名必须提交")
+    private String name;
+    /**
+     * 品牌logo地址
+     */
+    @NotEmpty
+    @URL(message = "logo必须是一个合法的url地址")
+    private String logo;
+    /**
+     * 介绍
+     */
+    private String descript;
+    /**
+     * 显示状态[0-不显示；1-显示]
+     */
+    private Integer showStatus;
+    /**
+     * 检索首字母[a-zA-Z]
+     */
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z]$", message = "检索首字母必须是一个字母")
+    private String firstLetter;
+    /**
+     * 排序
+     */
+    @NotNull
+    @Min(value = 0, message = "排序必须大于等于0")
+    private Integer sort;
 
 }
