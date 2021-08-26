59c9050627ec04e6ac3baf1c8108247daa6494f7
dali <daki9981@qq.com>
2021-08-26 16:24:58 +0800

    修改showStatus显示状态分组校验空接口

---
+++ gulimall-common/src/main/java/com/atguigu/common/valid/UpdateStatusGroup.java
@@ -0,0 +1,10 @@
+package com.atguigu.common.valid;
+
+/**
+ * @Author Dali
+ * @Date 2021/8/26 15:43
+ * @Version 1.0
+ * @Description: 修改showStatus显示状态分组校验空接口
+ */
+public interface UpdateStatusGroup {
+}
