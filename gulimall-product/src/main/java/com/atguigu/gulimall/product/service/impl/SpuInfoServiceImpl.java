08380516a22e7cc193e439e159c8bd64c4c36d31
dali <daki9981@qq.com>
2021-11-06 14:29:46 +0800

    P87、商品服务-API-新增商品-商品新增业务流程分析

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.vo.SpuSaveVo;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +14,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.SpuInfoDao;
 import com.atguigu.gulimall.product.entity.SpuInfoEntity;
 import com.atguigu.gulimall.product.service.SpuInfoService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("spuInfoService")
@@ -24,6 +28,30 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 保存前端传送的spu信息，可能会保存很多，所以我们使用事务注解@Transactional
+     *
+     * @param vo
+     */
+    @Transactional
+    @Override
+    public void saveSpuInfo(SpuSaveVo vo) {
+        //1、保存spu基本信息：pms_spu_info
+        //2、保存spu的描述图片：spm_spu_info_desc
+
+        //3、保存spu的规格参数：pms_product_attr_value
+
+        //4、保存spu的规格参数：pms_product_attr_value
+
+        //5、保存spu的积分信息：gulimall_sms ——》 sms_spu_bounds （跨库）
+
+        //5、保存当前spu对应的所有sku信息(需要跨库操作)
+        //5.1 保存sku的基本信息：pms_sku_info
+        //5.2 保存sku的图片信息：pms_sku_images
+        //5.3 保存sku的销售属性信息：pms_sku_sale_attr_value
+        //5.4 保存sku的优惠，满减等信息：gulimall_sms数据库中的sms_sku_ladder\sms_sku_full_reduction\
     }
 
 }
