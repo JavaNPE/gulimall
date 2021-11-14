c84184fc2a9e210d96bd9508d53ad712f0f949e2
dali <daki9981@qq.com>
2021-11-14 19:53:25 +0800

    P95、仓储服务-API-仓库管理整合ware服务&获取仓库列表：1、把gulimall-ware微服务添加到注册中心中

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -1,8 +1,14 @@
 package com.atguigu.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement    //开启事务
+@MapperScan("com.atguigu.gulimall.ware.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
 
