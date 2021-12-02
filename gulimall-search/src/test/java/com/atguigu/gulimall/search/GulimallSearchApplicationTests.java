2db2f9f8a9b0a85419978f0b516400ad72581fef
dali <daki9981@qq.com>
2021-12-02 20:41:59 +0800

    P127、 全文检索ElasticSearch.整合-测试复杂检索

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -4,6 +4,7 @@ package com.atguigu.gulimall.search;
 import com.alibaba.fastjson.JSON;
 import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
 import lombok.Data;
+import lombok.ToString;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
@@ -12,6 +13,13 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContent;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,8 +47,17 @@ public class GulimallSearchApplicationTests {
 //        sourceBuilder.from();
 //        sourceBuilder.size();
 //        sourceBuilder.aggregation();
-        sourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
-        System.out.println(sourceBuilder.toString());
+        sourceBuilder.query(QueryBuilders.matchQuery("address","mill"));
+        //AggregationBuilders工具类构建AggregationBuilder
+        // 构建第一个聚合条件:按照年龄的值分布
+        TermsAggregationBuilder agg1 = AggregationBuilders.terms("agg1").field("age").size(10);// 聚合名称
+// 参数为AggregationBuilder
+        sourceBuilder.aggregation(agg1);
+        // 构建第二个聚合条件:平均薪资
+        AvgAggregationBuilder agg2 = AggregationBuilders.avg("agg2").field("balance");
+        sourceBuilder.aggregation(agg2);
+
+        System.out.println("检索条件"+sourceBuilder.toString());
 
         searchRequest.source(sourceBuilder);
 
@@ -48,8 +65,45 @@ public class GulimallSearchApplicationTests {
         SearchResponse response = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
         // 3 分析响应结果
         System.out.println(response.toString());
-    }
 
+        // 3.1 获取java bean
+        SearchHits hits = response.getHits();
+        SearchHit[] hits1 = hits.getHits();
+        for (SearchHit hit : hits1) {
+            hit.getId();
+            hit.getIndex();
+            String sourceAsString = hit.getSourceAsString();
+            Account account = JSON.parseObject(sourceAsString, Account.class);
+            System.out.println(account);
+
+        }
+
+
+        // 3.2 获取检索到的分析信息
+        Aggregations aggregations = response.getAggregations();
+        Terms agg21 = aggregations.get("agg1");
+        for (Terms.Bucket bucket : agg21.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println(keyAsString);
+        }
+
+
+    }
+    @Data
+    @ToString
+    static class Account {
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private String age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
 
     /**
      * 测试存储数据到ES
