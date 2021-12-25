9e7831addf8e8c6134697c25981cec6f9056a48c
dali <daki9981@qq.com>
2021-12-25 18:44:04 +0800

    150、性能压测优化优化三级分类数据获取）代码格式化

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -33,135 +33,135 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     //方式二：由于CategoryServiceImpl继承ServiceImpl 存在泛型 只需要使用 baseMapper即可
     */
 
-    @Autowired
-    CategoryService categoryService;
+	@Autowired
+	CategoryService categoryService;
 
-    @Autowired
-    CategoryBrandRelationService categoryBrandRelationService;
+	@Autowired
+	CategoryBrandRelationService categoryBrandRelationService;
 
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        IPage<CategoryEntity> page = this.page(
-                new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
-        );
+	@Override
+	public PageUtils queryPage(Map<String, Object> params) {
+		IPage<CategoryEntity> page = this.page(
+				new Query<CategoryEntity>().getPage(params),
+				new QueryWrapper<CategoryEntity>()
+		);
 
-        return new PageUtils(page);
-    }
+		return new PageUtils(page);
+	}
 
-    @Override
-    public List<CategoryEntity> listWithTree() {
-        //1、查出所有分类
-        List<CategoryEntity> entities = baseMapper.selectList(null); //null表示没有查询条件，就表示查询所有
+	@Override
+	public List<CategoryEntity> listWithTree() {
+		//1、查出所有分类
+		List<CategoryEntity> entities = baseMapper.selectList(null); //null表示没有查询条件，就表示查询所有
 
-        //2、组装成父子的树形结构
-        //方式二：(方式一：需要自动注入CategoryDao)由于CategoryServiceImpl继承ServiceImpl 存在泛型 只需要使用 baseMapper即可
-        //2.1:找到所有的一级分类
-        List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity) -> {  //建 Stream方式一：通过集合创建 (categoryEntity)的括号可以省略
-            //Stream 的中间操作：filter(Predicate p) 接收 Lambda ， 从流中排除某些元素
-            return categoryEntity.getParentCid() == 0;  //因为只有一条返回语句 return和{}也可以省略
-            //Stream 的终止操作:收集:collect(Collector c)将流转换为其他形式。把流中元素收集到Lis，用于给Stream中元素做汇总的方法
-        }).map((menu) -> {
-            menu.setChildren(getChildrens(menu, entities));
-            return menu;
-        }).sorted((menu1, menu2) -> {
-            //处理空指针异常
-            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
-        }).collect(Collectors.toList());
+		//2、组装成父子的树形结构
+		//方式二：(方式一：需要自动注入CategoryDao)由于CategoryServiceImpl继承ServiceImpl 存在泛型 只需要使用 baseMapper即可
+		//2.1:找到所有的一级分类
+		List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity) -> {  //建 Stream方式一：通过集合创建 (categoryEntity)的括号可以省略
+			//Stream 的中间操作：filter(Predicate p) 接收 Lambda ， 从流中排除某些元素
+			return categoryEntity.getParentCid() == 0;  //因为只有一条返回语句 return和{}也可以省略
+			//Stream 的终止操作:收集:collect(Collector c)将流转换为其他形式。把流中元素收集到Lis，用于给Stream中元素做汇总的方法
+		}).map((menu) -> {
+			menu.setChildren(getChildrens(menu, entities));
+			return menu;
+		}).sorted((menu1, menu2) -> {
+			//处理空指针异常
+			return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+		}).collect(Collectors.toList());
 
-        return level1Menus;
-    }
+		return level1Menus;
+	}
 
-    @Override
-    public void removeMenuByIds(List<Long> asList) {
-        //TODO 检查当前删除的菜单，是否被别的地方引用
+	@Override
+	public void removeMenuByIds(List<Long> asList) {
+		//TODO 检查当前删除的菜单，是否被别的地方引用
 
-        //逻辑删除
-        baseMapper.deleteBatchIds(asList);
-    }
+		//逻辑删除
+		baseMapper.deleteBatchIds(asList);
+	}
 
-    //[2,25,225]
-    @Override
-    public Long[] findCatelogPath(Long catelogId) {
-        List<Long> paths = new ArrayList<>();
-        List<Long> parentPath = findParentPath(catelogId, paths);//查出来的集合{225,34,2}是逆序的
-        //使用Collections工具类将其逆序转换
-        Collections.reverse(parentPath);    //完整路径：[2, 34, 225]
+	//[2,25,225]
+	@Override
+	public Long[] findCatelogPath(Long catelogId) {
+		List<Long> paths = new ArrayList<>();
+		List<Long> parentPath = findParentPath(catelogId, paths);//查出来的集合{225,34,2}是逆序的
+		//使用Collections工具类将其逆序转换
+		Collections.reverse(parentPath);    //完整路径：[2, 34, 225]
 
-        return parentPath.toArray(new Long[parentPath.size()]); //将list集合转成long[]数组
-    }
+		return parentPath.toArray(new Long[parentPath.size()]); //将list集合转成long[]数组
+	}
 
-    /**
-     * 级联更新所有关联的数据
-     * 方式一：通过xml文件格式操作数据库写法：目前报错：{"msg":"参数格式校验失败","code":10001}
-     *
-     * @param category
-     */
-    @Transactional  //这是一个事务= 更新自己还有更新级联的数据，所以加个事务
-    @Override
-    public void updateCascade(CategoryEntity category) {
-        //首先更新自己:一般都是通过this的方式通过id更新
-        this.updateById(category);    //视频中这么写的
+	/**
+	 * 级联更新所有关联的数据
+	 * 方式一：通过xml文件格式操作数据库写法：目前报错：{"msg":"参数格式校验失败","code":10001}
+	 *
+	 * @param category
+	 */
+	@Transactional  //这是一个事务= 更新自己还有更新级联的数据，所以加个事务
+	@Override
+	public void updateCascade(CategoryEntity category) {
+		//首先更新自己:一般都是通过this的方式通过id更新
+		this.updateById(category);    //视频中这么写的
 //        categoryService.updateById(category);
-        //更新关联表中的数据
-        categoryBrandRelationService.updateCategory(category.getCatId(), category.getName());
-    }
+		//更新关联表中的数据
+		categoryBrandRelationService.updateCategory(category.getCatId(), category.getName());
+	}
 
-    /**
-     * 查询所有的一级分类：
-     * "pms_category"表中parent_cid=0（没有上一级分类）或者cat_level=1表示为1级分类
-     *
-     * @return
-     */
-    @Override
-    public List<CategoryEntity> getLevel1Categorys() {
-        //selectList查询集合
-        List<CategoryEntity> categoryEntities = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
+	/**
+	 * 查询所有的一级分类：
+	 * "pms_category"表中parent_cid=0（没有上一级分类）或者cat_level=1表示为1级分类
+	 *
+	 * @return
+	 */
+	@Override
+	public List<CategoryEntity> getLevel1Categorys() {
+		//selectList查询集合
+		List<CategoryEntity> categoryEntities = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
 
-        return categoryEntities;
-    }
+		return categoryEntities;
+	}
 
-    /**
-     * 查出所有的分类
-     *
-     * @return
-     */
-    @Override
-    public Map<String, List<Catelog2Vo>> getCatalogJson() {
+	/**
+	 * 查出所有的分类
+	 *
+	 * @return
+	 */
+	@Override
+	public Map<String, List<Catelog2Vo>> getCatalogJson() {
 
-        //1、查出所有1级分类
-        List<CategoryEntity> level1Categorys = getLevel1Categorys();
+		//1、查出所有1级分类
+		List<CategoryEntity> level1Categorys = getLevel1Categorys();
 
-        //2、封装数据【重要】
-        Map<String, List<Catelog2Vo>> parent_cid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
-            //1、每一个的一级分类，查到这个一级分类的二级分类
-            List<CategoryEntity> categoryEntities = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", v.getCatId()));
+		//2、封装数据【重要】
+		Map<String, List<Catelog2Vo>> parent_cid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+			//1、每一个的一级分类，查到这个一级分类的二级分类
+			List<CategoryEntity> categoryEntities = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", v.getCatId()));
 
-            //2、封装上面的结果
-            List<Catelog2Vo> catelog2Vos = null;
-            if (categoryEntities != null) {
-                catelog2Vos = categoryEntities.stream().map(l2 -> {
-                    Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
-                    //1、找当前二级分类的三级分类，封装成vo
-                    List<CategoryEntity> level3Catelog = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", l2.getCatId()));
-                    if (level3Catelog != null) {
-                        List<Catelog2Vo.Catelog3Vo> collect = level3Catelog.stream().map(l3 -> {
-                            //2、封装成指定格式
-                            Catelog2Vo.Catelog3Vo catelog3Vo = new Catelog2Vo.Catelog3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
+			//2、封装上面的结果
+			List<Catelog2Vo> catelog2Vos = null;
+			if (categoryEntities != null) {
+				catelog2Vos = categoryEntities.stream().map(l2 -> {
+					Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
+					//1、找当前二级分类的三级分类，封装成vo
+					List<CategoryEntity> level3Catelog = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", l2.getCatId()));
+					if (level3Catelog != null) {
+						List<Catelog2Vo.Catelog3Vo> collect = level3Catelog.stream().map(l3 -> {
+							//2、封装成指定格式
+							Catelog2Vo.Catelog3Vo catelog3Vo = new Catelog2Vo.Catelog3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
 
-                            return catelog3Vo;
-                        }).collect(Collectors.toList());
+							return catelog3Vo;
+						}).collect(Collectors.toList());
 
-                        catelog2Vo.setCatalog3List(collect);
-                    }
+						catelog2Vo.setCatalog3List(collect);
+					}
 
-                    return catelog2Vo;
-                }).collect(Collectors.toList());
-            }
-            return catelog2Vos;
-        }));
-        return parent_cid;
-    }
+					return catelog2Vo;
+				}).collect(Collectors.toList());
+			}
+			return catelog2Vos;
+		}));
+		return parent_cid;
+	}
 
 //    /**
 //     * 级联更新所有关联的数据
@@ -180,35 +180,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 //        }
 //    }
 
-    //递归: 递归收集所有父分类， 比如我们现在是查手机{225,34,2}  手机/手机通讯/手机(225)
-    private List<Long> findParentPath(Long catelogId, List<Long> paths) {
-        //1、收集当前节点id
-        paths.add(catelogId);
-        CategoryEntity byId = this.getById(catelogId);
-        if (byId.getParentCid() != 0) { //如果当前节点有父id
-            findParentPath(byId.getParentCid(), paths);
-        }
-        return paths;
-    }
+	//递归: 递归收集所有父分类， 比如我们现在是查手机{225,34,2}  手机/手机通讯/手机(225)
+	private List<Long> findParentPath(Long catelogId, List<Long> paths) {
+		//1、收集当前节点id
+		paths.add(catelogId);
+		CategoryEntity byId = this.getById(catelogId);
+		if (byId.getParentCid() != 0) { //如果当前节点有父id
+			findParentPath(byId.getParentCid(), paths);
+		}
+		return paths;
+	}
 
-    /**
-     * 递归查找获取某一个菜单的子菜单
-     * 结合：空指针异常的处理方式：使用三目运算符
-     *
-     * @return
-     */
-    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {//(当前菜单，所有菜单)
-        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
-            return categoryEntity.getParentCid().equals(root.getCatId());
-        }).map(categoryEntity -> {
-            //1、找到子菜单
-            categoryEntity.setChildren(getChildrens(categoryEntity, all));
-            return categoryEntity;
-        }).sorted((menu1, menu2) -> {
-            //2、菜单的排序：处理空指针异常
-            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
-        }).collect(Collectors.toList());
-        return children;
+	/**
+	 * 递归查找获取某一个菜单的子菜单
+	 * 结合：空指针异常的处理方式：使用三目运算符
+	 *
+	 * @return
+	 */
+	private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {//(当前菜单，所有菜单)
+		List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+			return categoryEntity.getParentCid().equals(root.getCatId());
+		}).map(categoryEntity -> {
+			//1、找到子菜单
+			categoryEntity.setChildren(getChildrens(categoryEntity, all));
+			return categoryEntity;
+		}).sorted((menu1, menu2) -> {
+			//2、菜单的排序：处理空指针异常
+			return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+		}).collect(Collectors.toList());
+		return children;
 
-    }
+	}
 }
