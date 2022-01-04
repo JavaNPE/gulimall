e668cb88943f4521ef00db4dd02293e766d2179f
dali <daki9981@qq.com>
2022-01-04 21:35:26 +0800

    P160、 缓存分布式锁- Redisson-lock锁测试：基于Redis的Redisson分布式可重入锁RLock Java对象实现了java.util.concurrent.locks.Lock接口。

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -3,11 +3,12 @@ package com.atguigu.gulimall.product.web;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 import com.atguigu.gulimall.product.vo.Catelog2Vo;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -24,6 +25,9 @@ public class IndexController {
 
 	@Autowired
 	CategoryService categoryService;
+
+	@Autowired
+	RedissonClient redisson;
 
 	/**
 	 * 无论我们是访问：
@@ -62,6 +66,25 @@ public class IndexController {
 	@ResponseBody
 	@GetMapping("/hello")
 	public String hello() {
+		// 基于Redis的Redisson分布式可重入锁RLock
+		// Java对象实现了java.util.concurrent.locks.Lock接口。
+		// 1、获取一把锁，只要锁的名字一样，就是同一把锁
+		RLock lock = redisson.getLock("my-lock");
+
+		// 2、加锁
+		lock.lock();	// 阻塞式等待。 默认加的锁都是30s的时间
+		//1)、锁的自动续期，如果业务超长，运行期间自动给锁续上新的30s。不用担心业务时间长，锁自动过期被删掉
+		//2)、加锁的业务只要运行完成，就不会给当前锁续期，即使不手动解锁， 锁默认在30s以后自动删除。
+		try {
+			System.out.println("加锁成功，执行业务..." + Thread.currentThread().getId());
+			Thread.sleep(30000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			// 3、解锁	，加锁解锁代码没有运行，redisson会不会出现死锁
+			System.out.println("释放锁..." + Thread.currentThread().getId());
+			lock.unlock();
+		}
 		return "hello";
 	}
 }
