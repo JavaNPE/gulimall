07ca568d247e98852f8e4ac19b148e1f4e7870ce
dali <daki9981@qq.com>
2022-04-23 16:16:38 +0800

    P211、商城业务-认证服务环境搭建-新建Model初始化操作

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthServerApplication.class, args);
+    }
+
+}
