886d34de37dda0c54871bda46f492956dcef7190
dali <daki9981@qq.com>
2022-06-03 11:29:15 +0800

    P255、消息队列-SpringBoot整合RabbitMQ

--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,26 @@
 package com.atguigu.gulimall.order;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
 
+    @Autowired
+    AmqpAdmin amqpAdmin;
+    /**
+     * 1、测试RabbitMQ之如何通过代码的方式创建Exchange、Queue、Bingding绑定关系
+     *      方式一：使用RabbitAutoConfiguration自动配置中的AmqpAdmin进行创建
+     * 2、测试如何收发消息
+     */
     @Test
-    void contextLoads() {
+    public void createExchange() {
+
     }
 
 }
