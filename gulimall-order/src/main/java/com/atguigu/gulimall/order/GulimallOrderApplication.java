48682068c0f6f4de74bc8a2f9a8c21c48c4c79ef
HedianTea <daki9981@qq.com>
2022-09-15 15:55:45 +0800

    P263、业务-订单服务整合SpringSession

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 使用 RabbitMQ步骤
@@ -20,6 +21,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *  @RabbitListener:类+方法上(监听哪些队列)
  *  @RabbitHandler:标在方法上（重载区分不同的消息）
  */
+@EnableRedisHttpSession
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableRabbit
---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/ThreadPoolConfigProperties.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author HedianTea
+ * @email daki9981@qq.com
+ * @date 2022/9/15 11:12
+ */
+@ConfigurationProperties(prefix = "gulimall.thread")
+@Data
+public class ThreadPoolConfigProperties {
+    private int coreSize;
+    private int maxSize;
+    private long keepAliveTime;
+}
