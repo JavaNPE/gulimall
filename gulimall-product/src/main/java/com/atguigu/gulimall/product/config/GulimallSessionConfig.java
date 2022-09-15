1b7059952e3bc972fca088b6135d67e7d0232764
HedianTea <daki9981@qq.com>
2022-09-15 11:05:39 +0800

    补充之前未学习内容：使用redis来管理我们的session-代码GulimallSessionConfig

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/GulimallSessionConfig.java
@@ -0,0 +1,33 @@
+package com.atguigu.gulimall.product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @author HedianTea
+ * @email daki9981@qq.com
+ * @date 2022/9/15 10:39
+ */
+@Configuration
+public class GulimallSessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+
+        DefaultCookieSerializer cookiesSerializer = new DefaultCookieSerializer();
+
+        cookiesSerializer.setDomainName("gulimall.com");
+        cookiesSerializer.setCookieName("GULISESSION");
+        return cookiesSerializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+}
